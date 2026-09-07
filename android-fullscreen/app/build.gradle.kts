d25e2e0277d7a6654903d9e7063a7afb06e2a913
Sehoba <128558332+Sehoba@users.noreply.github.com>
2026-09-07 20:03:42 +0200

    Configure MoebelSchroeder Android app #2

--- android-fullscreen/app/build.gradle.kts
+++ android-fullscreen/app/build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
 }
 
 android {
-    namespace = "de.moebelschroeder.app"
+    namespace = "de.moebelschroeder.app2"
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "de.moebelschroeder.app"
+        applicationId = "de.moebelschroeder.app2"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1.0-fullscreen"
+        versionCode = 1
+        versionName = "2.0.0-redesign-fullscreen"
     }
 }
